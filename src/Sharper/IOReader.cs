c63ac76ee98416a4da8ed848070517d38e2f4531
Blair Davidson <blair.joel.davidson@gmail.com>
2015-06-22 00:06:21 +0800

    Demo test sample, Dictionary examples, Tests, Option IO methods

--- src/Sharper/IOReader.cs
+++ src/Sharper/IOReader.cs
@@ -5,22 +5,26 @@ namespace Sharper
 
     public class IOReader<A,B>
     {
-        public IOReader(IO<Reader<A,B>> n)
+        public IOReader(IO<Reader<A,B>> monad)
         {
-            this._n = n;
+            this.monad = monad;
         }
 
         public IOReader<A,C> Map<C>(Func<B,C> f)
         {
-            return _n.Map(z => z.Map(f)).ReaderT();
+            return monad.Map(z => z.Map(f)).ReaderT();
         }
 
         public IOReader<A,D> FlatMap<C,D>(Func<B, IOReader<A,D>> f)
         {
-            return null;
+            return new IO<Reader<A,D>>(() => {
+                var reader = monad.PerformUnsafeIO();
+                var g = reader.FlatMap(x => f(x).monad.PerformUnsafeIO());
+                return g;
+            }).ReaderT();
         }
 
-        private readonly IO<Reader<A,B>> _n;
+        private readonly IO<Reader<A,B>> monad;
     }
 
 }
